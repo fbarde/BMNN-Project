cd1cc2c4c7ce753ee419b0db8ebc69e4d6240256
fbarde <91252846+fbarde@users.noreply.github.com>
2022-05-23 16:56:11 +0200

    Debut integr. et modif passive prop.

--- Passive_properties.py
+++ Passive_properties.py
@@ -6,52 +6,62 @@ from implementation_HH import simulate_HH_neuron_adaptative
 from implementation_HH import plot_Vm_I
 
 
-def find_E_l():
+def find_E_l(plot=True):
     """Function to find the parameter E_l"""
     # No input current to find E_l
     current = input_factory.get_zero_current()
     state_monitor_adaptative = simulate_HH_neuron_adaptative(current, 100 * b2.ms)
-    plot_Vm_I(state_monitor_adaptative, r'Stimulation protocol: find parameter $E_l$.')
+    if(plot==True):
+        plot_Vm_I(state_monitor_adaptative, r'Stimulation protocol: find parameter $E_l$.')
     
     #E_l corresponds to the constant potential value that is reached
     E_l =  max(state_monitor_adaptative.vm[0])
-    print("Parameter E_l = ", E_l)
+    if(plot==True):
+        print("Parameter E_l = ", E_l)
     return E_l
 
-def find_g_l():
+
+def find_g_l(plot=True):
     """ Function to find parameter g_l"""
+    # C'EST 1/R !!!
 
     # Inject a very small input current such that the neuron doesn't fire
     I_ext = 0.1
     current = input_factory.get_step_current(10, 100, b2.ms, I_ext * b2.uA)
     state_monitor_adaptative = simulate_HH_neuron_adaptative(current, 100 * b2.ms)
-    plot_Vm_I(state_monitor_adaptative, r'Stimulation protocol: find parameter $g_l$.')
+    if(plot==True):
+        plot_Vm_I(state_monitor_adaptative, r'Stimulation protocol: find parameter $g_l$.')
 
-    E_l = find_E_l()
+    E_l = find_E_l(False)
     Vm_const = max(state_monitor_adaptative.vm[0])
 
-    g_l = I_ext*b2.uA/(Vm_const - E_l)
+    #g_l = I_ext*b2.uA/(Vm_const - E_l)
+    g_l=1/find_R(False)
 
-    print("Parameter g_l =" , g_l)
+    if(plot==True):
+        print("Parameter g_l =" , g_l)
     return g_l
 
-def find_R():
+def find_R(plot=True):
     """ Function to find parameter membrane resistance R"""
     # Need to input a delta current: active for only one timestep
+    # FAUX !! JE DOIS FAIRE UN STEP CURRENT !!
     I_ext = 1.0
-    current = input_factory.get_step_current(10, 10, b2.ms, I_ext * b2.uA)
-    state_monitor_adaptative = simulate_HH_neuron_adaptative(current, 50 * b2.ms)
-    plot_Vm_I(state_monitor_adaptative, r'Stimulation protocol: find parameter $R$.')
+    current = input_factory.get_step_current(10, 50, b2.ms, I_ext * b2.uA)
+    state_monitor_adaptative = simulate_HH_neuron_adaptative(current, 100 * b2.ms)
+    if(plot==True):
+        plot_Vm_I(state_monitor_adaptative, r'Stimulation protocol: find parameter $R$.')
 
     v_max = max(state_monitor_adaptative.vm[0])
     v_rest = -69.7* b2.mV
 
     R = (v_max - v_rest)/(I_ext * b2.uA)
-    print("Parameter resistance of the membrane R =" , R)
+    if(plot==True):
+        print("Parameter resistance of the membrane R =" , R)
     return R
 
 
-def find_tau_m():
+def find_tau_m(plot=True):
     """ Function to find parameter membrane time constant tau_m"""
     # Tau_m corresponds to the time it takes for the neuron to reach 63% of max v_m value
 
@@ -59,49 +69,52 @@ def find_tau_m():
     I_ext = 1.0
     current = input_factory.get_step_current(0, 100, b2.ms, I_ext * b2.uA)
     state_monitor_adaptative = simulate_HH_neuron_adaptative(current, 100 * b2.ms)
-    #plot_Vm_I(state_monitor_adaptative, r'Stimulation protocol: find parameter $\tau_m$.')
-
-    # Plot the figure with tau line
-    fig,ax = plt.subplots(2,1)
-    ax[0].plot(state_monitor_adaptative.t / b2.ms, state_monitor_adaptative.vm[0] / b2.mV, lw=2)
-    ax[0].set_xlabel("t [ms]")
-    ax[0].set_ylabel(r"$V_m$ [mV]")
-    ax[0].grid()
-    ax[0].hlines(y=-63.77, xmin=0, xmax=9.0, linewidth=1.8, color='r')
-    ax[0].vlines(x=9.0, ymin=-70, ymax=-63.77, linewidth=2, color='r')
-    ax[1].plot(state_monitor_adaptative.t / b2.ms, state_monitor_adaptative.I_e[0] / b2.uamp, "red", lw=2)
-    ax[1].set_xlabel("t [ms]")
-    ax[1].set_ylabel("$I_{e}$ [$\mu$ A]")
-    ax[1].grid()
-    plt.suptitle( r'Stimulation protocol: find parameter $\tau_m$.')
-    plt.show()
+   
+    if(plot==True):
+        # Plot the figure with tau line
+        fig,ax = plt.subplots(2,1)
+        ax[0].plot(state_monitor_adaptative.t / b2.ms, state_monitor_adaptative.vm[0] / b2.mV, lw=2)
+        ax[0].set_xlabel("t [ms]")
+        ax[0].set_ylabel(r"$V_m$ [mV]")
+        ax[0].grid()
+        ax[0].hlines(y=-63.77, xmin=0, xmax=9.0, linewidth=1.8, color='r')
+        ax[0].vlines(x=9.0, ymin=-70, ymax=-63.77, linewidth=2, color='r')
+        ax[1].plot(state_monitor_adaptative.t / b2.ms, state_monitor_adaptative.I_e[0] / b2.uamp, "red", lw=2)
+        ax[1].set_xlabel("t [ms]")
+        ax[1].set_ylabel("$I_{e}$ [$\mu$ A]")
+        ax[1].grid()
+        plt.suptitle( r'Stimulation protocol: find parameter $\tau_m$.')
+        plt.show()
 
 
     Vm_start = -70*b2.mV
     Vm_const = max(state_monitor_adaptative.vm[0])
     Vm_63 = Vm_start-(Vm_start-Vm_const)*0.63
-    print(Vm_63)
+    
     #idx_63 = np.where(state_monitor_adaptative.vm[0]==-63.77*b2.mV)[0]
     #print(idx_63)
     #tau_m = state_monitor_adaptative.t[idx_63]
     tau_m = 9.0 * b2.ms
-    print("Parameter time constant of the membrane tau_m =" , tau_m)
+    if(plot==True):
+        print(Vm_63)
+        print("Parameter time constant of the membrane tau_m =" , tau_m)
     return tau_m
 
-def find_C():
+def find_C(plot=True):
     """ Function to find parameter C """
     # C= tau_m/R
-    tau_m =find_tau_m()
-    R = find_R()
+    tau_m =find_tau_m(False)
+    R = find_R(False)
     C=tau_m/R
 
-    print("Parameter C =" , C)
+    if(plot==True):
+        print("Parameter C =" , C)
     return C
 
 
 if __name__ == "__main__":
-    find_E_l()
-    find_g_l()
-    find_R()
-    find_tau_m()
+    #find_E_l()
+    #find_g_l()
+    #find_R()
+    #find_tau_m()
     find_C()
